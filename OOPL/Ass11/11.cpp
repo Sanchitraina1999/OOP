9b48ac56dc9f922eb58650a0e2c5c9d9f763bdb7
Sanchitraina1999 <sanchitraina1999@gmail.com>
2019-10-16 19:38:20 +0530

    4

---
+++ OOPL/Ass11/11.cpp
@@ -0,0 +1,67 @@
+/*
+Write C++ program using STL for implementation of stack  & queue using SLL
+*/
+#include<iostream>
+#include<list>
+using namespace std;
+int main(){
+    list <int> S,Q;
+    list <int> :: iterator it1,it2;
+    int n;
+    hell:
+    cout<<"1.STACK\n2.QUEUE\n Enter the choice: ";
+    int choice; cin>>choice;
+    switch (choice)
+    {
+    case 1:
+        cout<<"1.PUSH \n2.POP \n3.DISPLAY STACK";
+        cin>>choice;
+        switch(choice){
+            case 1:
+                cout<<"Enter number to be pushed into the stack";
+                cin>>n;
+                S.push_back(n);
+                break;
+            case 2:
+                S.pop_back();
+                break;
+            case 3:
+                for(it1=S.begin();it1!=S.end();it1++)
+                    cout<<*it1<<" ";
+                cout<<endl;
+        }
+
+        break;
+    case 2:
+        cout << "1. PUSH BACK \n2. POP BACK\n3. PUSH FRONT \n4. POP FRONT\n5. DISPLAY QUEUE";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter number to be pushed into the queue at the BACK";
+            cin >> n;
+            Q.push_back(n);
+            break;
+        case 2:
+            Q.pop_back();
+            break;
+        case 3:
+            cout << "Enter number to be pushed into the queue at the FRONT";
+            cin >> n;
+            Q.push_front(n);
+            break;
+        case 4:
+            Q.pop_front();
+        case 5:
+            for (it2 = Q.begin(); it2 != Q.end(); it2++)
+                cout << *it2 << " ";
+            cout << endl;
+        }
+        break;
+    }
+    cout<<"Continue(y/n)? : ";
+    char yn;    cin>>yn;
+    if(yn=='y')
+        goto hell;
+    return 0;
+}
